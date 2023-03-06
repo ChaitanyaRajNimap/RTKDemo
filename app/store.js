139357d6f73918dc0214ab17478407f0e7e5dc41
ChaitanyaRajNimap <chaitanyarajiwade@nimapinfotech.com>
2023-03-06 10:25:23 +0530

    Async action called using RTK

--- app/store.js
+++ app/store.js
@@ -4,12 +4,14 @@ const configureStore = require("@reduxjs/toolkit").configureStore;
 // const { default: logger } = require("redux-logger");
 const cakeReducer = require("../features/cake/cakeSlice");
 const iceCreamReducer = require("../features/iceCream/iceCreamSlice");
+const userReducer = require("../features/user/userSlice");
 
 //Configuring / Defining store
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     iceCream: iceCreamReducer,
+    user: userReducer,
   },
   // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
 const iceCreamActions =
   require("./features/iceCream/iceCreamSlice").iceCreamActions;
+const fetchUsers = require("./features/user/userSlice").fetchUsers;
 
 console.log("Initial state : ", store.getState());
 
@@ -13,15 +14,17 @@ const unsubscribe = store.subscribe(() => {
   console.log("Updated state : ", store.getState());
 });
 
-//Dispatching actions
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(3));
+store.dispatch(fetchUsers());
 
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.restocked(2));
+//Dispatching actions
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(3));
+
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.restocked(2));
 
 //Unsubscribing store
-unsubscribe();
+// unsubscribe();
